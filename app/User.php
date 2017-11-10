aedd4415f2bb7cddbdae181bddda0fa2b007523f
Ruben Engels <ruben.engels@live.nl>
2017-11-10 11:05:37 +0100

    added login with owner, memeber and admin logins

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAdmin()
+    {
+        return $this->admin; // this looks for an admin column in your users table
+    }
+
+    public function isOwner()
+    {
+      return $this->owner; // this looks for an owner column in your users table
+    }
 }
