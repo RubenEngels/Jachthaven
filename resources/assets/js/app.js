d51360408006ff84cd299c0e1e8d908648f70384
Ruben Engels <ruben.engels@live.nl>
2018-06-13 13:42:36 +0200

    disable vue and update composer

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,7 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -16,7 +16,7 @@ window.Vue = require('vue');
  */
 
 // Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app',
-});
+// 
+// const app = new Vue({
+//     el: '#app',
+// });
